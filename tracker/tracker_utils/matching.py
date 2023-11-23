994e397f34a27b0902e7b03d7d8073c21a88cdcd
amish0 <amishkumar562@fmail.com>
2023-11-23 15:19:49 +0530

    moved all file to tracker folder

--- tracker/tracker_utils/matching.py
+++ tracker/tracker_utils/matching.py
@@ -4,50 +4,7 @@ import lap
 from scipy.spatial.distance import cdist
 
 # from cython_bbox import bbox_overlaps as bbox_ious
-from utils.metrics import bbox_ioa
-from Bot_Sort import kalman_filter
-
-
-def merge_matches(m1, m2, shape):
-    O,P,Q = shape
-    m1 = np.asarray(m1)
-    m2 = np.asarray(m2)
-
-    M1 = scipy.sparse.coo_matrix((np.ones(len(m1)), (m1[:, 0], m1[:, 1])), shape=(O, P))
-    M2 = scipy.sparse.coo_matrix((np.ones(len(m2)), (m2[:, 0], m2[:, 1])), shape=(P, Q))
-
-    mask = M1*M2
-    match = mask.nonzero()
-    match = list(zip(match[0], match[1]))
-    unmatched_O = tuple(set(range(O)) - set([i for i, j in match]))
-    unmatched_Q = tuple(set(range(Q)) - set([j for i, j in match]))
-
-    return match, unmatched_O, unmatched_Q
-
-
-def _indices_to_matches(cost_matrix, indices, thresh):
-    matched_cost = cost_matrix[tuple(zip(*indices))]
-    matched_mask = (matched_cost <= thresh)
-
-    matches = indices[matched_mask]
-    unmatched_a = tuple(set(range(cost_matrix.shape[0])) - set(matches[:, 0]))
-    unmatched_b = tuple(set(range(cost_matrix.shape[1])) - set(matches[:, 1]))
-
-    return matches, unmatched_a, unmatched_b
-
-
-# def linear_assignment(cost_matrix, thresh):
-#     if cost_matrix.size == 0:
-#         return np.empty((0, 2), dtype=int), tuple(range(cost_matrix.shape[0])), tuple(range(cost_matrix.shape[1]))
-#     matches, unmatched_a, unmatched_b = [], [], []
-#     cost, x, y = lap.lapjv(cost_matrix, extend_cost=True, cost_limit=thresh)
-#     for ix, mx in enumerate(x):
-#         if mx >= 0:
-#             matches.append([ix, mx])
-#     unmatched_a = np.where(x < 0)[0]
-#     unmatched_b = np.where(y < 0)[0]
-#     matches = np.asarray(matches)
-#     return matches, unmatched_a, unmatched_b
+from basic_utils.metrics import bbox_ioa
 
 ## taken help from ultralitsics v8
 def linear_assignment(cost_matrix, thresh, use_lap=True):
@@ -179,36 +136,6 @@ def embedding_distance(tracks, detections, metric='cosine'):
 
     cost_matrix = np.maximum(0.0, cdist(track_features, det_features, metric))  # / 2.0  # Nomalized features
     return cost_matrix
-
-
-def gate_cost_matrix(kf, cost_matrix, tracks, detections, only_position=False):
-    if cost_matrix.size == 0:
-        return cost_matrix
-    gating_dim = 2 if only_position else 4
-    gating_threshold = kalman_filter.chi2inv95[gating_dim]
-    # measurements = np.asarray([det.to_xyah() for det in detections])
-    measurements = np.asarray([det.to_xywh() for det in detections])
-    for row, track in enumerate(tracks):
-        gating_distance = kf.gating_distance(
-            track.mean, track.covariance, measurements, only_position)
-        cost_matrix[row, gating_distance > gating_threshold] = np.inf
-    return cost_matrix
-
-
-def fuse_motion(kf, cost_matrix, tracks, detections, only_position=False, lambda_=0.98):
-    if cost_matrix.size == 0:
-        return cost_matrix
-    gating_dim = 2 if only_position else 4
-    gating_threshold = kalman_filter.chi2inv95[gating_dim]
-    # measurements = np.asarray([det.to_xyah() for det in detections])
-    measurements = np.asarray([det.to_xywh() for det in detections])
-    for row, track in enumerate(tracks):
-        gating_distance = kf.gating_distance(
-            track.mean, track.covariance, measurements, only_position, metric='maha')
-        cost_matrix[row, gating_distance > gating_threshold] = np.inf
-        cost_matrix[row] = lambda_ * cost_matrix[row] + (1 - lambda_) * gating_distance
-    return cost_matrix
-
 
 def fuse_iou(cost_matrix, tracks, detections):
     if cost_matrix.size == 0:
